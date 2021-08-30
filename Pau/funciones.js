e5be7316061e2c1f8d6a021e9bd5809f6ee73c2e
Pau <pauu.linan@gmail.com>
2021-08-30 19:17:06 -0300

    Arreglo de directorio y links

--- Pau/funciones.js
+++
@@ -1,9 +0,0 @@
-//la funcion va a pedir ingresar el nombre del producto y va a calcular el iva 
-let zara = 1556;
-let anastasia = 2500;
-let monaco = 2800;
-
-while(true){
-    let precioConjunto = prompt("Ingrese el nombre del conjunto deseado: zara, anastasia o monaco");
-    if
-}
